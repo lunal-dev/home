06cdbc8bb3b5d242ff465a67e6fcc9126a3eca5e
geomatrick <patrick@geomatrick.io>
2026-03-13 12:20:06 -0400

    plausible added

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Lora, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 
@@ -25,6 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src="https://plausible.io/js/pa-fe_AMrp4xlNmw8myKYHux.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`}
+        </Script>
+      </head>
       <body className={`${lora.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <main className="px-6 md:px-10 py-16 max-w-[860px] mx-auto">
